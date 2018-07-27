d90dd427ed3fc8a8091102d51c726c15ff994194
Frédérick PAREJA <frederick.pareja.pf1@gmail.com>
2018-07-27 13:10:24 +0200

    first commit to opening project

---
+++ src/main/java/co/simplon/EverDesignBackApplication.java
@@ -0,0 +1,12 @@
+package co.simplon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverDesignBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverDesignBackApplication.class, args);
+	}
+}
